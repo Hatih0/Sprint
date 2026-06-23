1cca6f65c3a2cc96815232812eff5c759f32f1d5
hatif <fitahiana.andrianantenaina@gmail.com>
2026-06-23 08:57:22 +0300

    sprint2

---
+++ Sprint3.md
@@ -0,0 +1,11 @@
+creer anotation GetUrl() pour les methode dans controlle
+
+voir si l url est valide si valide afficher :
+
+lister les  methodes du controller posse avec  dans l affichage
+
+    - url		Nom Controller 		NomMethode
+
+ else 
+
+renvoyer exception (erreur)
--- src/main/itu/hatif/servlet/FrontControllerServlet.java
+++ src/main/itu/hatif/servlet/FrontControllerServlet.java
@@ -1,46 +1,65 @@
 package itu.hatif.servlet;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import itu.hatif.annotation.Controller;
+import itu.hatif.annotation.GetUrl;
+import itu.hatif.util.Mapping;
 import itu.hatif.util.Util;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
 public class FrontControllerServlet extends HttpServlet {
 
-    List<String> listeController;
+    Map<String,Mapping> mapUrlsControllers;
 
     public void init() {
 
-        listeController = new ArrayList<>();
+    mapUrlsControllers = new HashMap<>();
 
-        String ControllerPackage = getServletContext().getInitParameter("controller-package");
-        
+    String controllerPackage =
+            getServletContext().getInitParameter("controller-package");
+
         try {
 
-            List<Class<?>> listClasses = Util.getAllClasses(ControllerPackage);
+            List<Class<?>> listClasses =
+                    Util.getAllClasses(controllerPackage);
+
             for (Class<?> clazz : listClasses) {
-                
+
                 if (clazz.isAnnotationPresent(Controller.class)) {
-                    
-                    listeController.add(clazz.getName());
 
-                }       
+                    for (Method method : clazz.getDeclaredMethods()) {
 
+                        if (method.isAnnotationPresent(GetUrl.class)) {
+
+                            GetUrl annotation =
+                                    method.getAnnotation(GetUrl.class);
+
+                            String url = annotation.url();
+
+                            Mapping mapping = new Mapping(
+                                    clazz.getName(),
+                                    method.getName()
+                            );
+
+                            mapUrlsControllers.put(url, mapping);
+                        }
+                    }
+                }
             }
-            
-        } catch (Exception e) {
 
+        } catch (Exception e) {
             e.printStackTrace();
-        
         }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+ 
         ProcessRequest(request, response);
 
     }
@@ -53,24 +72,48 @@ public class FrontControllerServlet extends HttpServlet {
 
     public void ProcessRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                
+                        response.setContentType("text/html");
+                
+                        PrintWriter out = response.getWriter();
+                        out.println("<html>");
+                        out.println("<head><title>List Class Controller </title></head>");
+                        out.println("<body>");
+                        
+                        String url = request.getRequestURI().substring(request.getContextPath().length());
+                        Mapping mapping = mapUrlsControllers.get(url);
+                        
+        if(mapping != null) {
+            try {
+                Class<?> clazz = Class.forName(mapping.getClassName());
+                
+                        out.println("<h1> Controller : " + clazz.getSimpleName() + "</h1>");
 
-        response.setContentType("text/html");
+                        out.println("<h2> Method : </h2>");
+                        
+                        for (Method method : clazz.getDeclaredMethods()) {
+                            
+                            if (method.isAnnotationPresent(GetUrl.class)) {
+                                
+                                GetUrl annotation = method.getAnnotation(GetUrl.class);
+                                String urlMethod = annotation.url();
 
-        PrintWriter out = response.getWriter();
+                                out.println("<p> -> Method : " + method.getName() + " - URL : " + urlMethod + "</p>");
+                            }
+                        
+                        }
+                        
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    
+                    out.println("<h1> No controller found for URL: " + url + "</h1>");
+                    
+                }
 
-        out.println("<html>");
-        out.println("<head><title>List Class Controller </title></head>");
-        out.println("<body>");
-        out.println("<h1> List Class Controller : </h1>");
-
-        for (String ctrl : listeController) {
-            
-            out.println("<p>" + ctrl + "</p>");
-        
-        }
-        out.println("</body>");
-        out.println("</html>");
-
+            out.println("</body>");
+            out.println("</html>");
     }
 
 }
